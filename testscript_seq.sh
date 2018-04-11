043f8e43acc56d047a821eeda7e5b55475d103da
Jonathan Westin <jwestin@cs.umu.se>
2018-04-11 10:33:23 +0200

    Added highthreads data

--- testscript_seq.sh
+++ testscript_seq.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 counter=1
-for y in {20..500..10}
+
+for y in {50..500..50}
 do
-	java -jar client.jar $1 1000 60 $y $i $i
+	java -jar client.jar $1 1000 60 $y 6 6
 	counter=$((counter+1))
 done
